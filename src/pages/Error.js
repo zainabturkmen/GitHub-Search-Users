8363b02641cf227fc01e0cdeb4dbdccec1315c84
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-21 12:37:43 +0430

    react-router

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -8,6 +8,7 @@ const Error = () => {
       <div>
         <h1>404</h1>
         <h3>sorry, the page you tried cannot be found</h3>
+        <Link to="/">back to home</Link>
       </div>
     </Wrapper>
   );

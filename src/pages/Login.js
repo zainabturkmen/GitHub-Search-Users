a511dcd5761be8434e87b45fa11cb89b4f28ba29
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-21 12:57:51 +0430

    react-router

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import styled from 'styled-components';
-import loginImg from '../images/login-img.svg';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+import loginImg from "../images/login-img.svg";
+
 const Login = () => {
-  return <h2>login page</h2>;
+  return (
+    <Wrapper>
+      <div className="container"></div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;

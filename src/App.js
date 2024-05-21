c2b60ee827dff6aea2a621e7f73ac3d3764aea78
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-21 09:36:23 +0430

    react-router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route>
+      <Route path="/">
         <Dashboard></Dashboard>
       </Route>
       <Route>

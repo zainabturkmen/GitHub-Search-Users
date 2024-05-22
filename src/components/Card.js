ac7664050078435fc19db33f46e287096a7158ba
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-22 17:04:11 +0430

    Github user

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,7 +20,7 @@ const Card = () => {
         <img src={avatar_url} alt={name} />
         <div>
           <h4>{name}</h4>
-          <p>@{twitter_username} || </p>
+          <p>@{twitter_username} || "John doe"</p>
         </div>
       </header>
     </Wrapper>

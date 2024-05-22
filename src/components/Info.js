3ba47f02ae161e90d8f3bc8dc59ac248ce60b173
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-22 10:32:00 +0430

    Github user

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,8 @@ import { FiUsers, FiUserPlus } from "react-icons/fi";
 const UserInfo = () => {
   const { githubUser } = React.useContext(GithubContext);
   const { public_repos, followers, following, public_gists } = githubUser;
+
+  const
   return <h2>user info component:</h2>;
 };
 

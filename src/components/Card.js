5f485ac6f6203643252d0c0d95cb3b6a4246c0e8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-22 16:33:26 +0430

    Github user

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
 const Card = () => {
-  const {githubUser} = React.createContext(githubUser);
-  const {} = githubUser
+  const { githubUser } = React.createContext(githubUser);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
   return <h2>card component</h2>;
 };
 const Wrapper = styled.article`
@@ -15,7 +24,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: "user";
     position: absolute;
     top: 0;
     left: 0;

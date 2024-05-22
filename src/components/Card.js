d6e1300c6504ea7da29a189c96fea1f64618d9eb
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-22 17:28:54 +0430

    Github user

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,7 +22,9 @@ const Card = () => {
           <h4>{name}</h4>
           <p>@{twitter_username || "John doe"}</p>
         </div>
+        <a href={html_url}>follow</a>
       </header>
+      <p className="bio"></p>
     </Wrapper>
   );
 };

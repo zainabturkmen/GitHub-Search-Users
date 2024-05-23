85c12df6f76b9d9d533d9d9c7f7aa77ddc133ad1
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-23 16:15:43 +0430

    Github search user

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -27,7 +27,7 @@ const ChartComponent = ({ data }) => {
     dataSource: {
       // Chart Configuration
       chart: {
-        caption: "Stars",
+        caption: "Stars per Language",
         theme: "fusion",
         decimals: 0,
         pieRadius: "45%",

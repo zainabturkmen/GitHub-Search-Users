cf470ba26f06b3c1ca232c96dc9219623d4405ca
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-23 15:26:36 +0430

    Github search user

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,6 +6,8 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+
+  
   let languages = repos.reduce((total, item) => {
     const { language } = item;
     if (!language) return total;

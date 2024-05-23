c62603620b46b81aa59f1f604de96186b83db9f7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-23 17:44:55 +0430

    Github search user

--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,44 @@
-import React from 'react';
+// // STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Column3D = () => {
-  return <div>chart</div>;
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "Pie3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "45%",
+        paletteColors: "#bcbcd",
+      },
+
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;

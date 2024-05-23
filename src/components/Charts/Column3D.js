213c0f888a778d063e24af9f1b73d0b19256e56d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-23 17:46:16 +0430

    Github search user

--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -27,7 +27,7 @@ const ChartComponent = ({ data }) => {
     dataSource: {
       // Chart Configuration
       chart: {
-        caption: "Languages",
+        caption: "Most Popular",
         theme: "fusion",
         decimals: 0,
         pieRadius: "45%",

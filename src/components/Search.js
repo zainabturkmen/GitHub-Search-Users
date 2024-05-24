a026664ce92dff76b8481226b29f15a77fac9493
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-24 11:00:52 +0430

    Github search user

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  const [user, setUser] = React.useState("")
+  const [user, setUser] = React.useState("");
   // get things from global context
 
   const handleSubmit = (e) => {
     console.log(user);
-  }
-  return <section className='section'>
-    <Wrapper className="section-center">
-      <form onSubmit={handleSubmit}>
-        <div className='form-control'>
-          <MdSearch/>
-          <input type='text' placeholder='enter github user' />
-          <button type='submit'>Search</button>
-        </div>
-      </form>
-      <h3>requests: 60/60</h3>
-    </Wrapper>
-  </section>
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={handleSubmit}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+        <h3>requests: 60/60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`

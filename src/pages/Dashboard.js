e0c6c152d63625fca77f3932c161795aad065d64
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-25 20:10:00 +0430

    Github search user

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
 const Dashboard = () => {
-  const {isloading}  = React.useContext(GithubContext)
+  const { isloading } = React.useContext(GithubContext);
   return (
     <main>
       <Navbar></Navbar>

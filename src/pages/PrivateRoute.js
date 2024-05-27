d73c656d2979b6859d722f78faba6ab2220c2cb0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-27 18:50:54 +0430

    Github search user

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({children,...rest}) => {
+  
   return <h2>private route component</h2>;
 };
 export default PrivateRoute;

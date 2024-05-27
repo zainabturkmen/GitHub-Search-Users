09aec6d74c725af56832651d1e965aed4f74f6db
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-27 19:28:51 +0430

    Github search user

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({ children, ...rest }) => {
   const {isAuthenticated, user} = useAuth0() 
-  const isUser = false;
+  const isUser = isAuthenticated;
   return (
     <Route
       {...rest}

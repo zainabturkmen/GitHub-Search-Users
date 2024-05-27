0897cd5fc3a8e078eb15a08dae12a11152e49390
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-27 18:24:48 +0430

    Github search user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <Wrapper>
-      {isUser && user.picture && <img src=""/>}
+      {isUser && user.picture && <img src={user.picture}/>}
       <button onClick={loginWithRedirect}>login</button>
       <button
         onClick={() => {
